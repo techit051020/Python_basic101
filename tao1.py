8904272c6ce5f59a6acf359778dcf9ac879d50ad
techit007 <125782384+techit051020@users.noreply.github.com>
2023-02-25 20:40:29 +0700

    ีturtle เบื้องต้น

---
+++ tao1.py
@@ -0,0 +1,9 @@
+import turtle
+tao=turtle.Pen()
+tao.shape('turtle')
+for i in range(10):
+    goto(10,10)
+    tao.forward(99)
+    tao.right(36)
+
+turtle.done()
---
+++ tao2.py
@@ -0,0 +1,9 @@
+import turtle
+tao=turtle.Pen()
+tao.turtle
+
+for i in range(4):
+    tao.forward(100)
+    tao.right(90)
+
+tao.done()
